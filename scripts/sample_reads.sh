01a9b722574585883781832b378c84cfe2f5d9eb
Bora Uyar <bora.uyar@mdc-berlin.de>
2026-01-29 09:30:22 +0100

    add utility script to downsample a fastq library for testing purposes

---
+++ scripts/sample_reads.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Usage:
+#   chmod +x sample_reads.sh
+#   ./sample_reads.sh <input_folder> <output_folder> <num_lines> <extension>
+#
+# Example:
+#   ./sample_reads.sh raw_fastqs sampled_reads 100000 fastq.gz
+
+in="$1"; out="$2"; n="$3"; ext="$4"
+mkdir -p "$out"
+find "$in" -name "*.${ext}" -type f | while read -r f; do
+  b=$(basename "$f" ".${ext}")
+  echo "Extracting ${n} lines from ${f}"
+  zcat "$f" | head -n "$n" | gzip > "$out/${b}.${ext}"
+done
+
